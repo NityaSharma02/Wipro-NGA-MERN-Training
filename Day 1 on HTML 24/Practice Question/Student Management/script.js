c705e8303c3af88f1954cb7d103c276fed9466de
nitya sharma <nityasharma638@gmail.com>
2025-10-30 10:19:25 +0530

    Training

---
+++ Day 1 on HTML 24/Practice Question/Student Management/script.js
@@ -0,0 +1,4 @@
+document.getElementById("profileBtn").addEventListener("click", function () {
+  document.getElementById("welcomeSection").classList.toggle("hidden");
+  document.getElementById("profileSection").classList.toggle("hidden");
+});
